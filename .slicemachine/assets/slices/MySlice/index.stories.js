f9c9a7b2c37322ca09d6d5ccaffaed6be9cd1237
Alexandre Chamla <alexandrec@MacBook-Pro-de-Alexandre.local>
2021-09-21 11:21:57 +0200

    adding product page

---
+++ .slicemachine/assets/slices/MySlice/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/MySlice';
+
+export default {
+  title: 'slices/MySlice'
+}
+
+
+export const _DefaultSlice = () => <MyComponent slice={{"variation":"default-slice","name":"Default slice","slice_type":"my_slice","items":[],"primary":{"title":[{"type":"heading1","text":"Productize collaborative networks","spans":[]}],"description":[{"type":"paragraph","text":"Cillum aliqua tempor elit sit pariatur. Consectetur sunt duis officia in sit cillum proident excepteur amet commodo esse eiusmod velit proident. Sit cillum exercitation excepteur sit tempor veniam adipisicing laborum magna cupidatat consectetur eu cillum proident.","spans":[]}],"image":{"dimensions":{"width":900,"height":500},"alt":"Placeholder image","copyright":null,"url":"https://images.unsplash.com/photo-1587653915936-5623ea0b949a?w=900&h=500&fit=crop"}},"id":"_DefaultSlice"}} />
+_DefaultSlice.storyName = 'Default slice'
