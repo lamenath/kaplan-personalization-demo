66533d13d79d3807a4bb167d102a60365ecee095
Alexandre Chamla <alexandrec@MacBook-Pro-de-Alexandre.local>
2021-09-21 12:32:29 +0200

    adding product page

--- .slicemachine/assets/slices/HeroBanner/index.stories.js
+++ .slicemachine/assets/slices/HeroBanner/index.stories.js
@@ -5,5 +5,5 @@ export default {
 }
 
 
-export const _DefaultSlice = () => <MyComponent slice={{"variation":"default-slice","name":"Default slice","slice_type":"hero_banner","items":[],"primary":{"title":"e-enable impactful functionalities","description":"grow distributed action-items","image":{"dimensions":{"width":900,"height":500},"alt":"Placeholder image","copyright":null,"url":"https://images.unsplash.com/photo-1471897488648-5eae4ac6686b?w=900&h=500&fit=crop"},"CTA":"enhance strategic e-commerce"},"id":"_DefaultSlice"}} />
+export const _DefaultSlice = () => <MyComponent slice={{"variation":"default-slice","name":"Default slice","slice_type":"hero_banner","items":[],"primary":{"title":"aggregate efficient communities","description":"disintermediate robust web services","image":{"dimensions":{"width":900,"height":500},"alt":"Placeholder image","copyright":null,"url":"https://images.unsplash.com/photo-1515378960530-7c0da6231fb1?w=900&h=500&fit=crop"},"CTA":"disintermediate sexy e-business"},"id":"_DefaultSlice"}} />
 _DefaultSlice.storyName = 'Default slice'
