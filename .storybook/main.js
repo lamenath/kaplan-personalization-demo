905db4aae1565e9b867f16459f58c245ef728d16
Alexandre Chamla <alexandrec@mbpdealexandre.home>
2021-09-29 10:18:12 +0200

    fixing storybook issue with postcss8

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,13 @@
 const { getStoriesPaths } = require('slice-machine-ui/helpers/storybook')
 module.exports = {
+  addons: [   
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },],
   stories: [...getStoriesPaths(), "../styles/globals.css"]
 }
